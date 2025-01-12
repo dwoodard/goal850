37b574de33d111b68f3f0fcc0d977859b7c14038
Dustin Woodard <dustin.woodard@gmail.com>
2025-01-12 09:50:19 -0700

    feat: enhance CollectionSeeder to insert multiple collections with updated settings

--- database/seeders/CollectionSeeder.php
+++ database/seeders/CollectionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use DB;
 use Illuminate\Database\Seeder;
 
 class CollectionSeeder extends Seeder
@@ -11,35 +12,22 @@ class CollectionSeeder extends Seeder
      */
     public function run(): void
     {
-        \DB::table('collections')->insert([
-            'id' => 1,
-            'handle' => 'pages',
-            'title' => 'pages',
-            'settings' => json_encode([
-                'dated' => false,
-                'mount' => null,
-                'sites' => ['default'],
-                'slugs' => true,
-                'inject' => [],
-                'layout' => 'layout',
-                'routes' => '{parent_uri}/{slug}',
-                'sort_dir' => 'asc',
-                'template' => 'default',
-                'propagate' => false,
-                'revisions' => false,
-                'structure' => ['root' => true, 'slugs' => true],
-                'sort_field' => null,
-                'taxonomies' => null,
-                'search_index' => null,
-                'title_formats' => [],
-                'default_status' => true,
-                'origin_behavior' => 'select',
-                'preview_targets' => [['label' => 'Entry', 'format' => '{permalink}', 'refresh' => true]],
-                'past_date_behavior' => 'public',
-                'future_date_behavior' => 'private',
-            ]),
-            'created_at' => '2025-01-12 00:14:50',
-            'updated_at' => '2025-01-12 00:14:50',
-        ]);
+        DB::table('collections')->insert(
+            [
+                [
+                    'id' => 1,
+                    'handle' => 'pages',
+                    'title' => 'pages',
+                    'settings' => '{"dated": false, "mount": null, "sites": ["default"], "slugs": true, "inject": [], "layout": "layout", "routes": "{parent_uri}/{slug}", "sort_dir": "asc", "template": "default", "propagate": false, "revisions": false, "structure": {"root": true, "slugs": true}, "sort_field": null, "taxonomies": null, "search_index": null, "title_formats": [], "default_status": true, "origin_behavior": "select", "preview_targets": [{"label": "Entry", "format": "{permalink}", "refresh": true}], "past_date_behavior": "public", "future_date_behavior": "private"}',
+                ],
+                [
+                    'id' => 2,
+                    'handle' => 'posts',
+                    'title' => 'posts',
+                    'settings' => '{"dated": true, "mount": null, "sites": ["default"], "slugs": true, "inject": [], "layout": "layout", "routes": "blog/{slug}", "sort_dir": "desc", "template": "posts/show", "propagate": false, "revisions": false, "structure": {"root": true, "slugs": true}, "sort_field": null, "taxonomies": [], "search_index": null, "title_formats": [], "default_status": true, "origin_behavior": "select", "preview_targets": [{"id": "VIQkvhSJ", "label": "Entry", "format": "{permalink}", "refresh": true}], "past_date_behavior": "public", "future_date_behavior": "private"}',
+                ],
+            ]
+
+        );
     }
 }
