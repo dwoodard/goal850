0a2b4ae00f94cbadfb288c26ad2a67ae3bbfd61e
Dustin Woodard <dustin.woodard@gmail.com>
2025-01-12 17:04:43 -0700

    tailwind antlers

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -7,6 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/views/**/*.antlers.html',
         './resources/js/**/*.vue',
     ],
 
