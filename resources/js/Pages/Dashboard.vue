ba3f46461733962c4031836d1bc94fb1f6a55439
Dustin Woodard <dustin.woodard@gmail.com>
2025-02-05 18:59:12 -0700

    style: improve layout and structure of Dashboard component for better readability

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -18,25 +18,22 @@ const props = defineProps({
             <h2>Dashboard</h2>
         </template>
 
-        <div class="py-12 ">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div class="py-12">
+            <div class="mx-6 px-5">
+                <div class="bg-sky-500">You're logged in!</div>
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
-                    <div class="p-6">You're logged in!</div>
 
-                    <div
-                    v-if="props.user.is_admin"
-                    >
+
+                    <div v-if="props.user.is_admin">
                         <!-- check if user is admin -->
                         {{ props.user.is_admin ? 'You are an admin' : 'You are not an admin' }}
                     </div>
 
-                    <div>
-                        <div class="p-6">
-                            <h1>User Information</h1>
-                            <div class="mt-4">
-                                <p>Name: {{props.user.first_name}} {{props.user.last_name}}</p>
-                                <p>Email: {{ props.user.email }}</p>
-                            </div>
+                    <div class="">
+                        <h1>User Information</h1>
+                        <div class="mt-4">
+                            <p>Name: {{ props.user.first_name }} {{ props.user.last_name }}</p>
+                            <p>Email: {{ props.user.email }}</p>
                         </div>
                     </div>
                 </div>
