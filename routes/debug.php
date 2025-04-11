ab2d5ef0243556a646a5098fa9749efad323e3d6
Dustin Woodard <dustin.woodard@gmail.com>
2025-04-11 09:08:30 -0600

    refactor: remove unused debug routes and encapsulate info route within environment check

--- routes/debug.php
+++ routes/debug.php
@@ -2,16 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Debugging routes
-
-Route::get('/debug/collection', function () {
-    //
-})->name('debug');
-
-Route::get('/debug/info', function () {
-    return [
-        'app' => config('app.name'),
-        'environment' => app()->environment(),
-        'debug' => config('app.debug'),
-    ];
-});
+if (app()->environment('local')) {
+    Route::get('/debug/info', function () {
+        return [
+            'app' => config('app.name'),
+            'environment' => app()->environment(),
+            'debug' => config('app.debug'),
+        ];
+    });
+}
