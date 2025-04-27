6fb612efb2b35ac8d6d76d231204575c5ca4cc41
Dustin Woodard <dustin.woodard@gmail.com>
2025-04-26 22:23:41 -0600

    refactor: remove redundant array-credit-overview component from Dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -40,14 +40,6 @@ if (typeof window !== 'undefined') {
         :sandbox="sandbox"/>
     </div>
 
-    <div class=" flex flex-col justify-center">
-      <array-credit-overview
-        class="bg-white"
-        appKey="3F03D20E-5311-43D8-8A76-E4B5D77793BD"
-        userToken="AD45C4BF-5C0A-40B3-8A53-ED29D091FA11"
-        apiUrl="https://array.io"/>
-    </div>
-
     <div class="py-12">
       <div class="mx-6 px-5">
         <div class="bg-sky-500 p-4 text-white">
