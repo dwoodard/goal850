83a5d044c98cea99c4a84894d4417cf597e087b0
Dustin Woodard <dustin.woodard@gmail.com>
2025-04-27 16:52:15 -0600

    refactor: streamline imports and clean up unused props in AuthenticationKBA component

--- resources/js/array/AuthenticationKBA.vue
+++ resources/js/array/AuthenticationKBA.vue
@@ -12,12 +12,10 @@ import { useArrayScripts } from '@/composables/useArrayScripts'
 
 // add a mount hook to check if the script is loaded
 import { onMounted } from 'vue'
-import { router } from '@inertiajs/vue3'
-import { usePage } from '@inertiajs/vue3'
+import { router, usePage } from '@inertiajs/vue3'
 
 onMounted(() => {
   window.addEventListener('array-event', function arrayEvent(arrayEvent) {
-
     if (arrayEvent.detail.event === 'success') {
       router.post(route('registration.wizard.kba.store'), arrayEvent.detail.metadata)
     }
@@ -25,31 +23,20 @@ onMounted(() => {
     if (arrayEvent.detail.event === 'error') {
       console.log(arrayEvent.detail.metadata)
     }
-
   })
-
 })
 
 defineProps({
   userId: {
     type: String,
     required: true
-  },
-  apiUrl: {
-    type: String,
-    default: 'https://mock.array.io'
-  },
-  sandbox: {
-    type: Boolean,
-    default: function(){
-      return true
-    }
   }
-
 })
 
 const { props } = usePage()
 const appKey = props.array.appKey
+const apiUrl = props.array.apiUrl
+const sandbox = props.array.sandbox
 // const apiKey = props.array.apiKey ? props.array.apiKey : '3F03D20E-5311-43D8-8A76-E4B5D77793BD'
 // const userToken = props.auth.user ? props.auth.user.token : 'AD45C4BF-5C0A-40B3-8A53-ED29D091FA11'
 
