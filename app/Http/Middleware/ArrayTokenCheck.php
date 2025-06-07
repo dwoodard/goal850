4c09474b076923bfcf0688fc2360662e8d5dd042
Dustin Woodard <dustin.woodard@gmail.com>
2025-06-07 17:32:07 -0600

    Refactor ArrayTokenCheck middleware to streamline user authentication flow and remove unused API call

--- app/Http/Middleware/ArrayTokenCheck.php
+++ app/Http/Middleware/ArrayTokenCheck.php
@@ -31,36 +31,32 @@ class ArrayTokenCheck
             return Redirect::route('registration.wizard.kba');
         }
 
+        // $response = Http::withHeaders([
+        //     'accept' => 'application/json',
+        //     'x-array-server-token' => config('array.api_token'),
+        //     // 'x-array-user-token' => $user->array_user_token,
+        // ])->get(config('array.api_url').'/api/user/v2?userId='.$user->array_user_id);
+
+        // $array_user = $response->json();
+
+        // if the user is not authenticated, redirect to the registration wizard
         $response = Http::withHeaders([
             'accept' => 'application/json',
             'content-type' => 'application/json',
-            'x-array-server-token' => config('array.api_token'),
-            // 'x-array-user-token' => $user->array_user_token,
-        ])->get(config('array.api_url').'/api/user/v2?userId='.$user->array_user_id);
+            'x-array-user-token' => $user->array_user_token,
+        ])
+            ->post(config('array.api_url').'/api/authenticate/v2/usertoken', [
+                'appKey' => config('array.app_key'),
+                'userId' => $user->array_user_id,
+                'ttlInMinutes' => 3306,
+            ]);
 
-        $authenticated = $response->json('authenticated');
+        $token = $response->json('userToken');
 
-        // if the user is not authenticated, redirect to the registration wizard
-        if ($authenticated === 'false') {
-
-            $response = Http::withHeaders([
-                'accept' => 'application/json',
-                'content-type' => 'application/json',
-                'x-array-user-token' => $user->array_user_token,
-            ])
-                ->post(config('array.api_url').'/api/authenticate/v2/usertoken', [
-                    'appKey' => config('array.app_key'),
-                    'userId' => $user->array_user_id,
-                    'ttlInMinutes' => 3306,
-                ]);
-
-            $token = $response->json('userToken');
-
-            if ($token) {
-                $user->update([
-                    'array_user_token' => $token,
-                ]);
-            }
+        if ($token) {
+            $user->update([
+                'array_user_token' => $token,
+            ]);
         }
 
         // based on the response look for authenticated, true or false
