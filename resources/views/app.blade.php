c59a3d15689029fecb6af808b147ccbcfae5d62d
Dustin Woodard <dustin.woodard@gmail.com>
2025-06-08 07:55:49 -0600

    Add new favicon images and update site manifest
    - Added favicon images in 16x16, 32x32, and ICO formats.
    - Updated site.webmanifest to include new icon paths and basic app information.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,14 @@
         <meta property="og:type" content="website">
         <meta property="og:image" content="https://goal850.com/images/og/GOAL850-OGBackgrouund.jpeg">
 
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+
+
+
         <title inertia>{{ config('app.name', 'Goal 850') }}</title>
 
         <!-- Fonts -->
